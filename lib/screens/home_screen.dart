fdfec4beebac8347db2ae854e4b0ff44c7c72886
Harshpreet Singh <harshpreet.singh@successive.com>
2024-03-28 12:51:15 +0530

    28/03/2024

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_jailbreak_detection/flutter_jailbreak_detection.dart';
 import 'package:security_demo/screens/secure_storage_screen.dart';
+import 'package:security_demo/screens/vpn_detection.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,6 +11,25 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  bool isjailBroken = false;
+  bool isRoot = false;
+  bool isVpn = false;
+  getRootDetails() async {
+    isjailBroken = await FlutterJailbreakDetection.jailbroken;
+    isRoot = await FlutterJailbreakDetection.developerMode;
+  }
+
+  getVpnDetails() async {
+    isVpn = await CheckVpnConnection.isVpnActive();
+  }
+
+  @override
+  void initState() {
+    getVpnDetails();
+    getRootDetails();
+    super.initState();
+  }
+
   TextEditingController controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
@@ -18,6 +39,12 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            isRoot == true
+                ? const Text('Device Rooted')
+                : const Text('Device Not Rooted'),
+            isVpn == true
+                ? const Text('VPN connection found')
+                : const Text('VPN not connected'),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
               child: TextFormField(
@@ -30,6 +57,7 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             ElevatedButton(
                 onPressed: () {
+                  getVpnDetails();
                   SecureStorage().writeSecureData('name', controller.text);
                   controller.clear();
                 },
@@ -39,10 +67,9 @@ class _HomeScreenState extends State<HomeScreen> {
                   'Store Message',
                   style: TextStyle(color: Colors.white),
                 )),
-                ElevatedButton(
+            ElevatedButton(
                 onPressed: () {
                   SecureStorage().readSecureData('name');
-
                 },
                 style: const ButtonStyle(
                     backgroundColor: MaterialStatePropertyAll(Colors.blue)),
---
+++ lib/screens/vpn_detection.dart
@@ -0,0 +1,25 @@
+
+import 'dart:io';
+
+class CheckVpnConnection {
+
+  static Future<bool> isVpnActive() async {
+    bool isVpnActive;
+    List<NetworkInterface> interfaces = await NetworkInterface.list(
+        includeLoopback: false, type: InternetAddressType.any);
+    interfaces.isNotEmpty
+        ? isVpnActive = interfaces.any((interface) =>
+            interface.name.contains("tun") ||
+            interface.name.contains("ppp") ||
+            interface.name.contains("tap") ||
+            interface.name.contains("l2tp") ||
+            interface.name.contains("ipsec") ||
+            interface.name.contains("vpn") ||
+            interface.name.contains("wireguard") ||
+            interface.name.contains("openvpn") ||
+            interface.name.contains("softether") ||
+            interface.name.contains("pptp"))
+        : isVpnActive = false;
+    return isVpnActive;
+  }
+}
